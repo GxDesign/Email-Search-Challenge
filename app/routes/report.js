394b376683a872a9e0ac1e1f0d9fe5716bf7e485
Giancarlo <gian@Melissas-Air.home>
2016-09-15 21:18:25 -0400

    add callback logic to report

--- app/routes/report.js
+++ app/routes/report.js
@@ -4,13 +4,14 @@ export default Ember.Route.extend({
 	searchResults: Ember.inject.service('search-results'),
 
 	beforeModel: function(transition) {
+		var self = this; 
 		const searchResults = this.get('searchResults');
 		var applicationController = this.controllerFor('application');
 
 		// check if the user has an authToken. We would also check the validity of the token in a live app
 		if (!localStorage.authToken) {
 		  applicationController.set('savedTransition', transition);
-		  this.transitionTo('login');
+		  self.transitionTo('login');
 		} else {
 		    applicationController.login();  
 
